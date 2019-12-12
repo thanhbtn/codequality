ba12deb54bf6af24305f985111207e83a19e23ed
Erick Bajao <fbajao@gitlab.com>
2019-12-13 02:11:37 +0800

    Refactor back to using old comparison
    Looks like it's just zsh who is causing the
    newline character comparison issues with diff.

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ echo "1..3"
 failed=0
 step=1
 
-got="test/fixtures/gl-code-quality-report.json"
-expect="test/expect/gl-code-quality-report.json"
+got_file="test/fixtures/gl-code-quality-report.json"
+expected_file="test/expect/gl-code-quality-report.json"
 fixtures_path="$PWD/test/fixtures/"
 
 # Missing SOURCE_CODE env var
@@ -25,10 +25,10 @@ echo
 
 # Normal execution
 desc="Generate expected output"
-rm -f $got
+rm -f $got_file
 DEFAULT_FILES_PATH="$PWD/codeclimate_defaults" SOURCE_CODE=$fixtures_path ./run.sh $fixtures_path
 
-if test $? -eq 0 && diff "$got" "$expect"; then
+if test $? -eq 0 && diff "$got_file" "$expected_file"; then
   echo "ok $step - $desc"
 else
   echo "not ok $step - $desc"
@@ -39,11 +39,9 @@ echo
 
 # with defined REPORT_STDOUT
 desc="Send expected output to STDOUT"
-rm -f $got
-# Due to diff issues with newline chars comparison, we just forward the STDOUT output to a file first and then do the diff.
-REPORT_STDOUT=1 DEFAULT_FILES_PATH="$PWD/codeclimate_defaults" SOURCE_CODE="$fixtures_path" ./run.sh "$fixtures_path" > "$got"
+actual_output=$(REPORT_STDOUT=1 DEFAULT_FILES_PATH="$PWD/codeclimate_defaults" SOURCE_CODE="$fixtures_path" ./run.sh "$fixtures_path")
 
-if test $? -eq 0 && diff "$got" "$expect"; then
+if test $? -eq 0 && echo "$actual_output" | diff - "$expected_file"; then
   echo "ok $step - $desc"
 else
   echo "not ok $step - $desc"
